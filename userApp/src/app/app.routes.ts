8c28719a8b352c1958859196f5104304e6c2f8c2
Rodrigo Fraile Esteban <rofraiesjob@gmail.com>
2024-09-07 16:54:16 +0200

    Primeras rutas y header-footer

--- userApp/src/app/app.routes.ts
+++ userApp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch: 'full', redirectTo: 'home'},
+    {path: "home", component: HomeComponent},
+    {path: "user", redirectTo: 'home'},
+    {path: "user/1", component: UserProfileComponent},
+    {path: "newuser", component: NewUserComponent},
+    {path: "updateuser", redirectTo: 'home'},
+    {path: "updateuser/1", component: UpdateUserComponent},
+    {path: "**", redirectTo: 'home'}
+];
